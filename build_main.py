55590c49198bbf06d2032eb3180dc88ab955f143
Gweneph <12696697+EphDoering@users.noreply.github.com>
2022-07-11 19:29:05 -0400

    Testing out a build script to see if it'll work on a mac, doesn't yet work on linux

---
+++ build_main.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+venv = 'venv'
+
+venv_python = './' + venv
+
+if sys.platform == 'win32':
+	venv_python += '/Scripts/python3.exe'
+else:
+	venv_python += '/bin/python3'
+
+if not os.path.isdir('./'+venv):
+	os.system(sys.executable+' -m venv '+venv)
+	os.system(venv_python+' -m pip install -r requirements.txt pyinstaller')
+
+if os.path.isfile('main.spec'):
+	os.system(venv_python+' -m PyInstaller main.spec')
+else:
+	os.system(venv_python+' -m PyInstaller --onefile main.py')

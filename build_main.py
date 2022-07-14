992da8bc0554f4589acb4cc3fceeeb0a04f90f8c
Gweneph <12696697+EphDoering@users.noreply.github.com>
2022-07-14 09:57:53 -0400

    added more automation, now rebuilds from wiped venv. added condintionals, should hopefully build on windows too.

--- build_main.py
+++ build_main.py
@@ -10,11 +10,23 @@ if sys.platform == 'win32':
 else:
 	venv_python += '/bin/python3'
 
+linux_hidden_modules=['espeak','python_espeak-0.5.egg-info','speechd_config','speechd']
+system_packages="/usr/lib/python3/dist-packages/"
+hidden_imports=[]
+
 if not os.path.isdir('./'+venv):
 	os.system(sys.executable+' -m venv '+venv)
 	os.system(venv_python+' -m pip install -r requirements.txt pyinstaller')
+	if sys.platform == 'linux':
+		full_paths=' '.join([system_packages+mod for mod in linux_hidden_modules])
+		copy_cmd="cp -r "+full_paths+' ./'+venv+'/lib/python3.8/site-packages/'
+		print(copy_cmd)
+		os.system(copy_cmd)
+		hidden_imports+=linux_hidden_modules
+		
 
 if os.path.isfile('main.spec'):
 	os.system(venv_python+' -m PyInstaller main.spec')
 else:
-	os.system(venv_python+' -m PyInstaller --onefile --hidden-import=espeak --hidden-import=python_espeak-0.5.egg-info --hidden-import=speechd_config --hidden-import=speechd main.py')
+	hi="".join([' --hidden-import='+imp for imp in hidden_imports])
+	os.system(venv_python+' -m PyInstaller --onefile'+hi+' main.py')
